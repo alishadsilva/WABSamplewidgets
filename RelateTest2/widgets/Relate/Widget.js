439ab86a8d5e58820f3fd6341a9d1b04c82f2162
alishadsilva <50789722+alishadsilva@users.noreply.github.com>
2020-05-14 22:53:47 +0530

    Added a for loop

--- RelateTest2/widgets/Relate/Widget.js
+++ RelateTest2/widgets/Relate/Widget.js
@@ -67,59 +67,97 @@ function(declare,
     }, "reldata");
 
       var self= this.map
-      var store, grid, outFieldsNF;
+      var store, grid1, grid2;
 
-      var layout = [[
-      {'name': 'OBJECT ID', 'field': 'OBJECTID'},
-      {'name': 'CITY NAME', 'field': 'CITY_NAME'},
-      {'name': 'STATE NAME', 'field': 'STATE_NAME'},
-      {'name': 'TYPE', 'field': 'TYPE'},
-      {'name': 'POP1990', 'field': 'POP1990'}
+      var layout1 = [[
+      {'name': 'OBJECT ID', 'field': 'field1'},
+      {'name': 'COUNTY NAME', 'field': 'field2'},
+      {'name': 'STATE NAME', 'field': 'field3'},
+      {'name': 'AREA', 'field': 'field4'},
+      {'name': 'POP1990', 'field': 'field5'}
     ]];
 
+    var layout2 = [[
+      {'name': 'OBJECT ID', 'field': 'field6'},
+      {'name': 'CITY NAME', 'field': 'field7'},
+      {'name': 'STATE NAME', 'field': 'field8'},
+      {'name': 'TYPE', 'field': 'field9'},
+      {'name': 'POP1990', 'field': 'field10'}
+    ]];
       
       var featurelayer= self.getLayer("RelatedTables_4354")
 
-        var relquery= new RelationshipQuery()
+        var relquery1= new RelationshipQuery()
         relquery.outFields=['*']
-        relquery.relationshipId=1
+        relquery.relationshipId=0
         
         featurelayer.on("click", function(evt){
-          graphicAttributes=evt.graphic.attributes;
-          relquery.definitionExpression= "STATE_NAME=" +"'" + graphicAttributes.STATE_NAME +"'"
-          featurelayer.queryRelatedFeatures(relquery, function relatedRecords(data){
-            console.log(data)
-            var outFieldsNF = ["OBJECTID", "CITY_NAME", "STATE_NAME", "TYPE", "POP1990"]
-              dataNF = array.map(data[graphicAttributes.OBJECTID].features, function(feature) {
-                return {
-                // Step: Reference the attribute field values
-                "OBJECTID" : feature.attributes[outFieldsNF[0]],
-                "CITY_NAME" : feature.attributes[outFieldsNF[1]],
-                "STATE_NAME" : feature.attributes[outFieldsNF[2]],
-                "TYPE" : feature.attributes[outFieldsNF[3]],
-                "POP1990" : feature.attributes[outFieldsNF[4]]
-              }
-            });
-
-              var objectstore= new Memory({
+          for (var i = 0; i <= self._layers.RelatedTables_4354.relationships.length; i++) {
+            var relquery= new RelationshipQuery()
+            relquery.outFields=['*']
+            relquery.relationshipId=i
+            graphicAttributes=evt.graphic.attributes;
+            relquery.definitionExpression= "STATE_NAME=" +"'" + graphicAttributes.STATE_NAME +"'"
+            featurelayer.queryRelatedFeatures(relquery, function relatedRecords(data){
+              console.log(data)
+              if (i==0) {
+                var outFieldsNF = ["OBJECTID", "NAME", "STATE_NAME", "AREA", "POP1990"]
+                dataNF = array.map(data[graphicAttributes.OBJECTID].features, function(feature) {
+                  return {
+                  // Step: Reference the attribute field values
+                  "field1" : feature.attributes[outFieldsNF[0]],
+                  "field2" : feature.attributes[outFieldsNF[1]],
+                  "field3" : feature.attributes[outFieldsNF[2]],
+                  "field4" : feature.attributes[outFieldsNF[3]],
+                  "field5" : feature.attributes[outFieldsNF[4]]
+                }
+              });
+              var objectstore1= new Memory({
                 data: dataNF
               });
-
-              var store= new ObjectStore({objectStore: objectstore});
-              grid.setStore(store);
-              grid.resize();
-            })
+                
+              var store1= new ObjectStore({objectStore: objectstore1});
+              grid1.setStore(store1);
+              grid1.resize();
+              }
+              if (i==1) {
+                var outFieldsNF = ["OBJECTID", "CITY_NAME", "STATE_NAME", "TYPE", "POP1990"]
+                dataNF2 = array.map(data[graphicAttributes.OBJECTID].features, function(feature) {
+                  return {
+                  // Step: Reference the attribute field values
+                  "field6" : feature.attributes[outFieldsNF[0]],
+                  "field7" : feature.attributes[outFieldsNF[1]],
+                  "field8" : feature.attributes[outFieldsNF[2]],
+                  "field9" : feature.attributes[outFieldsNF[3]],
+                  "field10" : feature.attributes[outFieldsNF[4]]
+                }
+              });
+                var objectStore2= new Memory({
+                  data: dataNF2
+                })
+                var store2= new ObjectStore({objectStore: objectstore2});
+                grid2.setStore(store2)
+                grid2.resize();
+              }
+              })
+          }
         })
 
-        var grid= new EnhancedGrid({
-              id: "grid",
-              store: store,
-              structure: layout
+        var grid1= new EnhancedGrid({
+              id: "grid1",
+              store: store1,
+              structure: layout1
+            })
+
+        var grid2= new EnhancedGrid({
+              id: "grid1",
+              store: store2,
+              structure: layout2
             })
 
         var cp1 = new ContentPane({
           title: "Table 1",
-          content: grid,
+          content: grid1,
           style: "width: 375px; height: 370px;"
         });
         tc.addChild(cp1);
@@ -127,7 +165,7 @@ function(declare,
 
     var cp2 = new ContentPane({
          title: "Table 2",
-         content: "We are known for our drinks.",
+         content: grid2,
          style: "width: 375px; height: 370px;"
     });
     tc.addChild(cp2);
